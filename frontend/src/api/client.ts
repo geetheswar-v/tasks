6c98b8cb7205ec13ce8c85e5af1330ee1caf64c8
ManohaDarla2 <112201034@smail.iitpkd.ac.in>
2026-01-22 00:39:27 +0530

    small url change

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://tasks-api.geetheswar.dev',
+  baseURL: import.meta.env.VITE_API_URL || 'https://tasks-api.geetheswar.dev',
   headers: {
     'Content-Type': 'application/json',
   },
